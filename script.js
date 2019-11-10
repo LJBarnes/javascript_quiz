24e7354f8d7d0c39b4622bb1279047a59fee21da
Leah Barnes <johnson.leahe@gmail.com>
2019-11-10 13:10:39 -0500

    updating js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// setEventListener for start button
-// when you click it, it starts the for loop with questions
 var startBtn = document.querySelector(".startButton");
 var startDiv = document.querySelector(".quizStart");
 var highScore = document.querySelector(".highScoreView");
@@ -9,164 +7,120 @@ var rightOrWrong = document.querySelector("#answerDisplay");
 var questionDisplay = document.querySelector("#questionh2");
 var questionIndex = 0;
 var timeInterval
-var timeLeft = questions.length *15;
+var timeLeft = questions.length * 15;
 var gameOverDiv = document.querySelector("#gameOverDiv");
 var gameend = false;
 var inputBox = document.querySelector("#initials");
 var saveScoresDiv = document.querySelector("#saveScoresDiv");
 var scoreHere = document.querySelector("#scoreHere");
-
-// var names = []
+var names = [];
 // var times = []
-// var saveButton = document.querySelector(".saveButton");
-// var initialInput = document.querySelector(".initialInput");
-// var highScoresDiv = document.querySelector("#highScoresDiv");
+
 
 // Landing Page/Start Quiz:
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz() {
 
-gameend = false;
-  var timeInterval = setInterval(function () {
-    timer.textContent = ("Time: ") + timeLeft;
-    timeLeft--;
-    timeText.textContent = "";
+    gameend = false;
+    var timeInterval = setInterval(function () {
+        timer.textContent = ("Time: ") + timeLeft;
+        timeLeft--;
+        timeText.textContent = "";
 
-    if (timeLeft === 0) {
-      timer.textContent = "Time: 0";
-      clearInterval(timeInterval);
-    }
-    if (gameend === true){
-      clearInterval(timeInterval);
-      }
-       
-  }, 1000);
+        if (timeLeft === 0) {
+            timer.textContent = "Time: 0";
+            clearInterval(timeInterval);
+        }
+        if (gameend === true) {
+            clearInterval(timeInterval);
+        }
 
-  displayQuestion();
-  // choiceSelected();
+    }, 1000);
+
+    displayQuestion();
+    
 }
 
 function displayQuestion() {
-  var question = questions[questionIndex];
-  questionDisplay.textContent = question.title;
-  document.getElementById("popUpBody").textContent = "";
-  for (var i = 0; i < question.choices.length; i++) {
-    var createButton = document.createElement("Button");
-    document.getElementById("popUpBody").appendChild(createButton);
-    createButton.setAttribute("class", "answerBtn");
-    createButton.innerHTML = question.choices[i];
-    // this runs choiceSelected() when a button is clicked--need to put in global scope?
-    createButton.addEventListener('click', choiceSelected);
-  }
+    var question = questions[questionIndex];
+    questionDisplay.textContent = question.title;
+    document.getElementById("popUpBody").textContent = "";
+    for (var i = 0; i < question.choices.length; i++) {
+        var createButton = document.createElement("Button");
+        document.getElementById("popUpBody").appendChild(createButton);
+        createButton.setAttribute("class", "answerBtn");
+        createButton.innerHTML = question.choices[i];
+        // this runs choiceSelected() when a button is clicked--need to put in global scope?
+        createButton.addEventListener('click', choiceSelected);
+    }
 
 }
 
 function choiceSelected(event) {
-  var question = questions[questionIndex];
-  var answer = question.answer;
-  var selectedAnswer = event.target;
-  
-  questionIndex++;
-  if (questionIndex >= questions.length) {
-       gameend =true;
-      endOfGame();
-    return;
-  }
+    var question = questions[questionIndex];
+    var answer = question.answer;
+    var selectedAnswer = event.target;
 
-  if (selectedAnswer.innerHTML === answer) {
-    // display "Right :)" in question/answer Div
-    rightOrWrong.textContent = "Right :)";
-  }
- 
-  else {
-    // display "Wrong :(" in question/answer Div
-    timeLeft = timeLeft - 5;
-    rightOrWrong.textContent = "Wrong :(";
-  }
-  
-  console.log(event.target);
-  console.log("correct answer: " + answer);
+    questionIndex++;
+    if (questionIndex >= questions.length) {
+        gameend = true;
+        endOfGame();
+        return;
+    }
 
-  displayQuestion();
-  // endOfGame();
+    if (selectedAnswer.innerHTML === answer) {
+        // display "Right :)" in question/answer Div
+        rightOrWrong.textContent = "Right :)";
+    }
+
+    else {
+        // display "Wrong :(" in question/answer Div
+        timeLeft = timeLeft - 5;
+        rightOrWrong.textContent = "Wrong :(";
+    }
+
+    console.log(event.target);
+    console.log("correct answer: " + answer);
+
+    displayQuestion();
 }
 
 function endOfGame() {
-  questionh2.style.display="none";
-  popUpBody.style.display="none";
-  answerDisplay.style.display="none";
-  gameOverDiv.style.display="block";
-  scoreHere.textContent = "Your Score is: " + timeLeft;
-  
-  // var initialInput = document.createElement("INPUT");
-  // initialInput.setAttribute("type", "text");
-  // initialInput.placeholder = "Type your initials here!";
-  // document.getElementById("gameOverDisplay").appendChild(initialInput);
-//   var saveButton = document.createElement("Button");
-//   document.getElementById("gameOverDiv").appendChild(saveButton);
-//   saveButton.setAttribute("class", "saveButton");
-//   saveButton.innerHTML = "Save Score";
-  // createButton.addEventListener('click', saveScore);
+    questionh2.style.display = "none";
+    popUpBody.style.display = "none";
+    answerDisplay.style.display = "none";
+    gameOverDiv.style.display = "block";
+    scoreHere.textContent = "Your Score is: " + timeLeft;
+   
 }
 
-// function saveScore() {
-//       // Stringify and save score in localStorage
-//     localStorage.setItem("times", JSON.stringify(times));
-//   }
 
-//   function saveName(){
-//     localStorage.setItem("names",JSON.stringify(names));
-//   }
-
-//   // When save button is clicked
-//   saveButton.addEventListener("submit", function(event) {
-//     event.preventDefault();
-  
-//     var initialText = initialInput.value.trim();
-
-//     // return if submitted text is blank
-//     if (initialText === "") {
-//       return;
-//     }
-
-//     names.push(initialText);
-//     initialInput.value = "";
-//     times.push(secondsLeft);
-//     saveName();
-//     saveScore();
-//     gameOverDiv.style.display = "none";
-
-    // displayHighScores();
-
-  // })
-
-//   function displayHighScores() {
-//     scores.innerHTML = "";
-//     times.innerHTML = "";
-//     resultsBox.style.display = "inline";
-//     resultsBox.style.visibility = "visible"
-//     for (var i = 0; i < names.length; i++) {
-//         var name = names[i];
-//         var time = times[i];
-//         var lin = document.createElement("li");
-//         var liT = document.createElement("li");
-//         lin.textContent = "NAME: " + name;
-//         liT.textContent = " TIME: " + time;
-
-//         scores.appendChild(lin);
-//         plTime.appendChild(liT);
-//     }
-
-
-// }
-
-  
+function storeInitials() {
+    //stringify and set "names" in localStorage
+    localStorage.setItem("names", JSON.stringify(names));
+}
+    inputBox.addEventListener("submit", function (event) {
+        event.preventDefault();
+        var initialText = inputBox.value.trim();
+    
+            //  add new initialText to names array, clear input...
+        names.push(initialText);
+        // inputBox.value = "";
+    
+        storeInitials();
+        renderInitials();
+    });
 
 
 
 
+function renderInitals() {
+    var storedNames = JSON.parse(localStorage.getItem("names"));
+    var newScore = localStorage.getItem("newScore")
+    savedHighScores.textContent = newScore;
 
+    }
 
 //DONE-set an event listener on the buttons to check for the correct answer
 // DONE-if the answer is incorrect display incorrect and subtract 5 seconds
@@ -203,23 +157,14 @@ function endOfGame() {
 
 
 //   MODAL for questions:
-// var closeBtn = document.querySelector(".xOut");
 var modal = document.querySelector(".modal");
 
 // closeBtn.addEventListener("click", close);
 startBtn.addEventListener("click", displayModal);
 
 function displayModal() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
-// function close() {
-//     modal.style.display = "none";
-//     clearTimeout(timeInterval);
-//   }
-
-
-// Need to add an event listener for answer buttons. Not sure how to determine what happens if the answer is wrong or right. Do I have to write out for each button what happens if clicked? Seems like it might need a loop of some sort, since it's an array. 
-
 
 
 console.log("working!");
